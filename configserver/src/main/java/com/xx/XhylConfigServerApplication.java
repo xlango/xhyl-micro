8cb58e6a163754af593bfcd1453c73cbf6d65497
xx132917 <32615513+xx132917@users.noreply.github.com>
2019-11-21 18:30:17 +0800

    first commit  --configserver

---
+++ configserver/src/main/java/com/xx/XhylConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.xx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class XhylConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XhylConfigServerApplication.class, args);
+    }
+
+}
